fe6d54ae3b709fd88152f841629729bc55f779d1
León Domingo <leon@riddleandcode.com>
2018-11-08 12:00:31 +0100

    Some adaptations for R&C

--- js/snooper.js
+++ js/snooper.js
@@ -1,129 +1,133 @@
 class BigchainDBSnooper {
-    constructor(baseUrl) {
-        this.url = baseUrl + "/api/v1/"
-    }
+  constructor(baseUrl) {
+    this.url = baseUrl + "/api/v1/"
+  }
 
-    queryAssets(queryString, callback) {
-        this.sendRequest("assets/?search=" + queryString, callback);
-    }
+  queryAssets(queryString, callback) {
+    // this.sendRequest("assets/?search=" + queryString, callback);
+    // instead of "assets"
+    this.sendRequest("metadata/?search=" + queryString, callback)
+  }
 
-    getTransactions(assetId, callback) {
-        this.sendRequest("transactions?asset_id=" + assetId, callback);
-    }
+  getTransactions(assetId, callback) {
+    this.sendRequest("transactions?asset_id=" + assetId, callback);
+  }
 
-    sendRequest(path, callback) {
-        let request = new XMLHttpRequest();
+  sendRequest(path, callback) {
+    let request = new XMLHttpRequest();
 
-        request.onreadystatechange = () => {
+    request.onreadystatechange = () => {
 
-            // Check if the request succeeded
-            if (request.status == 200 && request.readyState == 4) {
-                callback(JSON.parse(request.response));
-            }
-        };
+      // Check if the request succeeded
+      if (request.status === 200 && request.readyState === 4) {
+        callback(JSON.parse(request.response));
+      }
+    };
 
-        request.open('GET', this.url + path);
-        request.send();
-    }
+    request.open('GET', this.url + path);
+    request.send();
+  }
 }
 
-var snooper = new Vue({
-    el: "#snooper",
-    data: {
-        snooperApi: new BigchainDBSnooper("https://test.bigchaindb.com"),
-        assets: new Array(),
-        transactions: new Array(),
+/* global Vue */
+const BDB_URL = 'http://ipdb-eu1.riddleandcode.com:9984'
+const snooper = new Vue({
+  el: "#snooper",
+  data: {
+    snooperApi: new BigchainDBSnooper(BDB_URL),
+    assets: new Array(),
+    transactions: new Array(),
 
-        // Settings
-        showSettings: false,
-        urlInput: "",
+    // Settings
+    showSettings: false,
+    urlInput: "",
 
-        // Delay query
-        isTyping: false,
-        assetsLoading: false,
-        transactionsLoading: false,
+    // Delay query
+    isTyping: false,
+    assetsLoading: false,
+    transactionsLoading: false,
 
-        // Transaction snooping
-        currentTransaction: undefined,
+    // Transaction snooping
+    currentTransaction: undefined,
 
-        // Input fields
-        searchInput: "",
+    // Input fields
+    searchInput: "",
+  },
+  methods: {
+    resetSearchInput() {
+      this.searchInput = "";
+      this.assets = new Array();
+      this.transactions = new Array();
+      this.currentTransaction = undefined;
     },
-    methods: {
-        resetSearchInput() {
-            this.searchInput = "";
-            this.assets = new Array();
-            this.transactions = new Array();
-            this.currentTransaction = undefined;
-        },
-        onAssetQueryResponse(response) {
-            this.stopLoading("assets");
-            for (asset in response)
-            {
-                console.log(JSON.stringify(response[asset].data, null, 2));
-            }
-            this.assets = response;
-        },
-        onTransactionsResponse(response) {
-            this.stopLoading("transactions");
-            this.transactions = response;
-        },
-        startLoading(item) {
-            switch (item) {
-                case "assets":
-                    this.assetsLoading = true;
-                    break;
+    onAssetQueryResponse(response) {
+      this.stopLoading("assets");
+      for (let asset in response) {
+        console.log(JSON.stringify(response[asset].data, null, 2));
+      }
 
-                case "transactions":
-                    this.transactionsLoading = true;
-                    break;
-            }
-        },
-        stopLoading(item) {
-            switch (item) {
-                case "assets":
-                    this.assetsLoading = false;
-                    break;
+      this.assets = response;
+    },
+    onTransactionsResponse(response) {
+      this.stopLoading("transactions");
+      this.transactions = response;
+    },
+    startLoading(item) {
+      switch (item) {
+        case "assets":
+          this.assetsLoading = true;
+          break;
 
-                case "transactions":
-                    this.transactionsLoading = false;
-                    break;
-            }
+        case "transactions":
+          this.transactionsLoading = true;
+          break;
+      }
+    },
+    stopLoading(item) {
+      switch (item) {
+        case "assets":
+          this.assetsLoading = false;
+          break;
 
-        },
-        onSearchInputChange() {
-            if (this.searchInput == "") {
-                this.stopLoading("assets");
-                this.assets = new Array();
-            }
-            else {
-                this.assets = new Array();
-                this.startLoading("assets");
-                this.isTyping = true;
+        case "transactions":
+          this.transactionsLoading = false;
+          break;
+      }
 
-                setTimeout(() => {
-                    this.isTyping = false;
-                }, 500);
-                setTimeout(() => {
-                    this.requestAssetQuery();
-                }, 1000);
-            }
-        },
-        requestAssetQuery() {
-            if (!this.isTyping)
-                this.snooperApi.queryAssets(this.searchInput, this.onAssetQueryResponse);
-        },
-        displayAssetTransactions(assetId) {
-            this.startLoading("transactions");
-            this.transactions = new Array();
-            this.snooperApi.getTransactions(assetId, this.onTransactionsResponse);
-        },
-        displayTransactionDetails(transaction) {
-            this.currentTransaction = transaction;
-        },
-        saveSettings() {
-            this.snooperApi = new BigchainDBSnooper(this.urlInput);
-            setTimeout(function() { this.showSettings = false; }.bind(this), 1000);
-        }
+    },
+    onSearchInputChange() {
+      if (this.searchInput === "") {
+        this.stopLoading("assets");
+        this.assets = new Array();
+      }
+      else {
+        this.assets = new Array();
+        this.startLoading("assets");
+        this.isTyping = true;
+
+        setTimeout(() => {
+          this.isTyping = false;
+        }, 500);
+        setTimeout(() => {
+          this.requestAssetQuery();
+        }, 1000);
+      }
+    },
+    requestAssetQuery() {
+      if (!this.isTyping)
+        this.snooperApi.queryAssets(this.searchInput, this.onAssetQueryResponse);
+    },
+    displayAssetTransactions(assetId) {
+      this.startLoading("transactions");
+      this.transactions = new Array();
+      this.snooperApi.getTransactions(assetId, this.onTransactionsResponse);
+    },
+    displayTransactionDetails(transaction) {
+      this.currentTransaction = transaction;
+    },
+    saveSettings() {
+      this.snooperApi = new BigchainDBSnooper(this.urlInput);
+      setTimeout(function() { this.showSettings = false; }.bind(this), 1000);
     }
+  }
 });
